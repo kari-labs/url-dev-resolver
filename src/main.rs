bf0b7a70d55c9d33047c8f083a22d6d959aab47a
Kyle <arow411@gmail.com>
2019-05-10 17:08:59 -0400

    Use hyper

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 extern crate futures;
 extern crate hyper;
 
-use std::net::{TcpListener, TcpStream};
-use std::io::{Read, Write, BufReader, BufRead};
 use futures::future;
 use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::rt::Future;
@@ -10,51 +8,10 @@ use hyper::header::{HeaderValue, LOCATION};
 use hyper::service::service_fn;
 
 fn main() {
-    tcpstreams()
+    hyper()
 }
 
-fn tcpstreams() {
-    let listener = TcpListener::bind("0.0.0.0:3001").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
-}
-
-fn handle_connection(mut stream: TcpStream) {
-    println!("handling connection");
-    
-    let mut buffer = [0; 512];
-
-    let mut reader = BufReader::new(stream);
-    
-    let mut line = String::new();
-    for rline in reader.by_ref().lines() {
-        line = rline.unwrap();
-        break;
-    }
-
-    let len = line.len();
-    let path: &str = if len > 14 {
-        line[5..len-9].as_ref()
-    } else {
-        "http://www.google.com/"
-    };
-
-    //let response = format!("HTTP/1.1 308 Permanent Redirect\r\nLocation: {}", path);
-    let response = "HTTP/1.1 308 Permanent Redirect\r\nLocation: http://www.google.com/";
-
-
-    stream = reader.into_inner();
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-
-}
-
-pub fn hyper() {
+fn hyper() {
     let addr = ([0, 0, 0, 0], 3001).into();
 
     let server = Server::bind(&addr)
